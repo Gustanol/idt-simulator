1cde7c2c3dfeab1e933f05ca93dc1828e5d92346
Gustanol <gualeixos456@gmail.com>
2025-09-19 09:54:09 -0300

    feat: adding two new functions: 'init_non_block_input' to disable buffered inputs and 'restore_terminal_settings' to restore old default settings

--- include/signal.h
+++ include/signal.h
@@ -1,5 +1,6 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
+#include <termios.h>
 
 /*
  * struct to define handlers
@@ -10,6 +11,22 @@ typedef struct {
     void (*f)(void);
 } IDT;
 
+static struct termios old_tio, new_tio;
+
+/*
+ * Termios struct
+ struct termios {
+    tcflag_t c_iflag;  // input flags
+    tcflag_t c_oflag;  // output flags
+    tcflag_t c_cflag;  // control flags
+    tcflag_t c_lflag;  // local flags
+    cc_t c_cc[NCCS];   // control characters
+    ...
+ }
+ */
+
 void run_program(void);
+void init_non_block_input(void);
+void restore_terminal_settings(void);
 
 #endif /* SIGNAL_H */
--- src/signal.c
+++ src/signal.c
@@ -1,3 +1,36 @@
+#include <pthread.h>
+#include <termios.h>
+#include <unistd.h>
+
 #include "signal.h"
 
-void run_program(void) {}
+static struct termios old_tio, new_tio;
+
+void init_non_block_input(void) {
+    /*
+     * read all current terminal settings and store it into old_tio (terminal input/output)
+     */
+    tcgetattr(STDIN_FILENO, &old_tio);
+    new_tio = old_tio;
+
+    /*
+     * disable canonical mode - buffered input (expects no longer for enter key to register
+     * commands)
+     */
+    new_tio.c_lflag &= ~ICANON;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);  // apply settings
+}
+
+void restore_terminal_settings(void) {
+    /*
+     * restore terminal settings using data stored in old_tio struct
+     */
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
+}
+
+void run_program(void) {
+    /*
+     * call function to init non-block terminal input
+     */
+    init_non_block_input();
+}
